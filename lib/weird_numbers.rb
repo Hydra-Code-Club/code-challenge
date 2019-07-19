c32f84e0d1cfddab7036cc8e81ddea50f031693c
Anna Headley <anna3LC@gmail.com>
2019-07-19 10:51:53 -0400

    Add cache, still too slow for 4030, try another sum test method

--- lib/weird_numbers.rb
+++ lib/weird_numbers.rb
@@ -24,35 +24,66 @@ def factors_of(n)
     result.uniq
 end
 
-def weird?(n)
+def weird?(n, debug=false)
+    debug = true if n == 4030
     factors = factors_of(n)
     #remove n from list of divisors
     factors.delete(n)
+    puts "factors: #{factors}" if debug
     return false if factors.sum <= n
-    return false if sum_to_n?(factors.sort.reverse, n)
+    summer = Summer.new(debug: debug)
+    return false if summer.new_sum_to_n?(factors.sort.reverse, n)
     true
 end
 
-def sum_to_n?(factors, n)
-    factors.each do |x|
-        if x == n
-            return true
+class Summer
+    def initialize(debug: false)
+        @cache = {}
+        @debug = debug
+    end
+
+    def new_sum_to_n?(factors, n)
+        (0..factors.length).each do |l|
+          factors.combination(l).each do |b|
+            #puts b.join(" ")
+            return true if b.sum == n
+          end
         end
-        if x < n
-            remaining_factors = factors.clone
-            remaining_factors.delete x
-            if sum_to_n?(remaining_factors, n - x)
+        false
+    end
+
+    def sum_to_n?(factors, n)
+        if @cache.keys.include?(factors)
+            #puts "Cache hit: #{factors}: #{@cache[factors]}" if @debug
+            return @cache[factors]
+        else
+            puts "Cache miss: n: #{n}, factors: #{factors}" if @debug
+        end
+
+        factors.each do |x|
+            if x == n
+                @cache[factors] = true
                 return true
             end
+            if x < n
+                remaining_factors = factors.clone
+                remaining_factors.delete x
+                if sum_to_n?(remaining_factors, n - x)
+                    @cache[factors] = true
+                    return true
+                end
+            end
         end
+        @cache[factors] = false
+        false
     end
-    false
 end
 
 n = ARGV[0].to_i
 (1..n).each do |x|
+    #puts "...#{x}" if (x % 100).zero?
+    #puts "...#{x}" if x > 4000
     if weird?(x)
         puts "#{x} is weird"
-        puts factors_of(x)
     end
 end
